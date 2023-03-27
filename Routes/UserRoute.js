fc4a6d6303a9ad146a06516ad6cbdafa230bfe07
squirtlelive <naa1.20284855@gmail.com>
2023-03-28 00:17:30 +0530

    update

--- Routes/UserRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { deleteUser, followUser, getUser, unfollowUser, updateUser } from "../Controllers/UserController.js";
-const router = express.Router();
-router.get('/:id', getUser)
-router.put('/:id', updateUser)
-router.delete('/:id', deleteUser)
-router.put('/:id/follow', followUser)
-router.put('/:id/unfollow', unfollowUser)
-export default router;
